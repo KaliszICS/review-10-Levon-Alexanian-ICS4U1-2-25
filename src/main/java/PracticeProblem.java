8666799daa4e42aabccf3d1f2fa122affa67bca1
Levon-Alexanian-ICS4U1-2-25 <levon.alexanian@student.tdsb.on.ca>
2025-03-25 03:59:31 +0000

    update PracticeProblem.java

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,45 @@
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static String pyramid(int rows) {
+        String pyramid = "";
+        for (int i = 1; i <= rows; i++) {
+			for (int j = 0; j < rows - i; j++) {
+                pyramid += " ";
+            }
+            for (int j = 0; j < i; j++) {
+                pyramid += "*";
+                if (j < i - 1) {
+                    pyramid += " ";
+                }
+            }
+			if (i != rows) {
+				pyramid = pyramid + "\n";
+			}
+		}
+        return pyramid;
+    }
 
-	}
+	public static String square(int size) {
+		String square = "";
+		for (int i = 0; i < size; i++) {
+            for (int n = 0; n < size; n++) {
+				if (i == 0 || i == size - 1 || n == 0 || n == size-1) {
+                	square += "*";
+            	} else {
+					square += " ";
+				}
 
+			}
+			square += "\n";
+        }
+        return square.strip();
+    }
+	public static boolean hasLowercase(String word) {
+        if (word == word.toUpperCase()) {
+            return false;
+        }
+        return true;
 }
+	
+}
+
